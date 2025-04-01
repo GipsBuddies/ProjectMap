e10677cfde88962338dc02b9e5f60d45165ac8a9
Lars Kerste <laj.kerste@student.avans.nl>
2025-04-01 16:04:10 +0200

    Unittest Doctor name is an actual name complete

---
+++ TestProjectApi/DoctorNameTest.cs
@@ -0,0 +1,67 @@
+using Microsoft.Testing.Platform.Logging;
+using Moq;
+using ProjectMap.WebApi.Controllers;
+using ProjectMap.WebApi.Repositories;
+using ProjectMap.WebApi.Interfaces;
+using ProjectMap.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestProjectApi;
+
+[TestClass]
+public class DoctorNameTest
+{
+    [TestMethod]
+    public async Task AddingDoctorNameWithActualNameShouldReturnCreatedResult()
+    {
+        // Arrange
+        Guid userId = Guid.NewGuid();
+        var MockChoiceRouteRepository = new Mock<IChoiceRouteRepository>();
+        
+        var mockAuthenticationService = new Mock<IAuthenticationService>();
+        mockAuthenticationService.Setup(authenticationService => authenticationService.GetCurrentAuthenticatedUserId())
+                    .Returns(userId.ToString);
+
+        var logger = new Mock<Microsoft.Extensions.Logging.ILogger<ChoiceRouteController>>();
+        var ChoiceRouteController = new ChoiceRouteController(MockChoiceRouteRepository.Object, logger.Object, mockAuthenticationService.Object);
+        
+        ChoiceRouteModel choiceRoute = new ChoiceRouteModel
+        {
+            UserId = userId,
+            NameDoctor = "name"
+        };
+
+        // Act
+        var response = await ChoiceRouteController.Add(choiceRoute);
+        // Assert
+        Assert.IsInstanceOfType(response, out CreatedResult createdObjectResult);
+    }
+
+
+
+    [TestMethod]
+    public async Task AddingDoctorNameWithEmptyStringShouldReturnBadRequest()
+    {
+        // Arrange
+        Guid userId = Guid.NewGuid();
+        var MockChoiceRouteRepository = new Mock<IChoiceRouteRepository>();
+
+        var mockAuthenticationService = new Mock<IAuthenticationService>();
+        mockAuthenticationService.Setup(authenticationService => authenticationService.GetCurrentAuthenticatedUserId())
+                    .Returns(userId.ToString);
+
+        var logger = new Mock<Microsoft.Extensions.Logging.ILogger<ChoiceRouteController>>();
+        var ChoiceRouteController = new ChoiceRouteController(MockChoiceRouteRepository.Object, logger.Object, mockAuthenticationService.Object);
+
+        ChoiceRouteModel choiceRoute = new ChoiceRouteModel
+        {
+            UserId = userId,
+            NameDoctor = ""
+        };
+
+        // Act
+        var response = await ChoiceRouteController.Add(choiceRoute);
+        // Assert
+        Assert.IsInstanceOfType(response, out BadRequestResult createdObjectResult);
+    }
+}
